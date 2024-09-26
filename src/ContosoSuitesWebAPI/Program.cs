7d2e3762e1d0cf7ad18ced1a290a4f412f70e5a8
ketanvh <ketanvh@gmail.com>
2024-09-26 16:01:09 -0500

    first commit

---
+++ src/ContosoSuitesWebAPI/Program.cs
@@ -0,0 +1,112 @@
+using Azure.Identity;
+using Microsoft.Azure.Cosmos;
+using ContosoSuitesWebAPI.Agents;
+using ContosoSuitesWebAPI.Entities;
+using ContosoSuitesWebAPI.Plugins;
+using ContosoSuitesWebAPI.Services;
+using Microsoft.Data.SqlClient;
+using Azure.AI.OpenAI;
+using Azure;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IDatabaseService, DatabaseService>();
+builder.Services.AddSingleton<IVectorizationService, VectorizationService>();
+builder.Services.AddSingleton<MaintenanceCopilot, MaintenanceCopilot>();
+
+builder.Services.AddSingleton<CosmosClient>((_) =>
+{
+    CosmosClient client = new(
+        connectionString: builder.Configuration["CosmosDB:ConnectionString"]!
+    );
+    return client;
+});
+
+builder.Services.AddSingleton<AzureOpenAIClient>((_) =>
+{
+    var endpoint = new Uri(builder.Configuration["AzureOpenAI:Endpoint"]!);
+    var credentials = new AzureKeyCredential(builder.Configuration["AzureOpenAI:ApiKey"]!);
+
+    var client = new AzureOpenAIClient(endpoint, credentials);
+    return client;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/", async () => 
+{
+    return "Welcome to the Contoso Suites Web API!";
+})
+    .WithName("Index")
+    .WithOpenApi();
+
+app.MapGet("/Hotels", async () => 
+{
+    throw new NotImplementedException();
+})
+    .WithName("GetHotels")
+    .WithOpenApi();
+
+app.MapGet("/Hotels/{hotelId}/Bookings/", async (int hotelId) => 
+{
+    throw new NotImplementedException();
+})
+    .WithName("GetBookingsForHotel")
+    .WithOpenApi();
+
+app.MapGet("/Hotels/{hotelId}/Bookings/{min_date}", async (int hotelId, DateTime min_date) => 
+{
+    throw new NotImplementedException();
+})
+    .WithName("GetRecentBookingsForHotel")
+    .WithOpenApi();
+
+app.MapPost("/Chat", async Task<string> (HttpRequest request) =>
+{
+    var message = await Task.FromResult(request.Form["message"]);
+    
+    return "This endpoint is not yet available.";
+})
+    .WithName("Chat")
+    .WithOpenApi();
+
+app.MapGet("/Vectorize", async (string text, [FromServices] IVectorizationService vectorizationService) =>
+{
+    var embeddings = await vectorizationService.GetEmbeddings(text);
+    return embeddings;
+})
+    .WithName("Vectorize")
+    .WithOpenApi();
+
+app.MapPost("/VectorSearch", async ([FromBody] float[] queryVector, [FromServices] IVectorizationService vectorizationService, int max_results = 0, double minimum_similarity_score = 0.8) =>
+{
+    // Exercise 3 Task 3 TODO #3: Insert code to call the ExecuteVectorSearch function on the Vectorization Service. Don't forget to remove the NotImplementedException.
+    throw new NotImplementedException();
+})
+    .WithName("VectorSearch")
+    .WithOpenApi();
+
+app.MapPost("/MaintenanceCopilotChat", async ([FromBody]string message, [FromServices] MaintenanceCopilot copilot) =>
+{
+    // Exercise 5 Task 2 TODO #10: Insert code to call the Chat function on the MaintenanceCopilot. Don't forget to remove the NotImplementedException.
+    throw new NotImplementedException();
+})
+    .WithName("Copilot")
+    .WithOpenApi();
+
+app.Run();
---
+++ src/ContosoSuitesWebAPI/Services/DatabaseService.cs
@@ -0,0 +1,90 @@
+using System.Runtime.CompilerServices;
+using Microsoft.Data.SqlClient;
+using ContosoSuitesWebAPI.Entities;
+
+namespace ContosoSuitesWebAPI.Services;
+
+public class DatabaseService : IDatabaseService
+{
+    public async Task<IEnumerable<Hotel>> GetHotels()
+    {
+        var sql = "SELECT HotelID, HotelName, City, Country FROM dbo.Hotel";
+        using var conn = new SqlConnection(
+            connectionString: Environment.GetEnvironmentVariable("SQLCONNSTR_ContosoSuites")!
+        );
+        conn.Open();
+        using var cmd = new SqlCommand(sql, conn);
+        using var reader = await cmd.ExecuteReaderAsync();
+        var hotels = new List<Hotel>();
+        while (await reader.ReadAsync())
+        {
+            hotels.Add(new Hotel
+            {
+                HotelID = reader.GetInt32(0),
+                HotelName = reader.GetString(1),
+                City = reader.GetString(2),
+                Country = reader.GetString(3)
+            });
+        }
+        conn.Close();
+
+        return hotels;
+    }
+
+    public async Task<IEnumerable<Booking>> GetBookingsForHotel(int hotelId)
+    {
+        var sql = "SELECT BookingID, CustomerID, HotelID, StayBeginDate, StayEndDate, NumberOfGuests FROM dbo.Booking WHERE HotelID = @HotelID";
+        using var conn = new SqlConnection(
+            connectionString: Environment.GetEnvironmentVariable("SQLCONNSTR_ContosoSuites")!
+        );
+        conn.Open();
+        using var cmd = new SqlCommand(sql, conn);
+        cmd.Parameters.AddWithValue("@HotelID", hotelId);
+        using var reader = await cmd.ExecuteReaderAsync();
+        var bookings = new List<Booking>();
+        while (await reader.ReadAsync())
+        {
+            bookings.Add(new Booking
+            {
+                BookingID = reader.GetInt32(0),
+                CustomerID = reader.GetInt32(1),
+                HotelID = reader.GetInt32(2),
+                StayBeginDate = reader.GetDateTime(3),
+                StayEndDate = reader.GetDateTime(4),
+                NumberOfGuests = reader.GetInt32(5)
+            });
+        }
+        conn.Close();
+
+        return bookings;
+    }
+
+    public async Task<IEnumerable<Booking>> GetBookingsByHotelAndMinimumDate(int hotelId, DateTime dt)
+    {
+        var sql = "SELECT BookingID, CustomerID, HotelID, StayBeginDate, StayEndDate, NumberOfGuests FROM dbo.Booking WHERE HotelID = @HotelID AND StayBeginDate >= @StayBeginDate";
+        using var conn = new SqlConnection(
+            connectionString: Environment.GetEnvironmentVariable("SQLCONNSTR_ContosoSuites")!
+        );
+        conn.Open();
+        using var cmd = new SqlCommand(sql, conn);
+        cmd.Parameters.AddWithValue("@HotelID", hotelId);
+        cmd.Parameters.AddWithValue("@StayBeginDate", dt);
+        using var reader = await cmd.ExecuteReaderAsync();
+        var bookings = new List<Booking>();
+        while (await reader.ReadAsync())
+        {
+            bookings.Add(new Booking
+            {
+                BookingID = reader.GetInt32(0),
+                CustomerID = reader.GetInt32(1),
+                HotelID = reader.GetInt32(2),
+                StayBeginDate = reader.GetDateTime(3),
+                StayEndDate = reader.GetDateTime(4),
+                NumberOfGuests = reader.GetInt32(5)
+            });
+        }
+        conn.Close();
+
+        return bookings;
+    }
+}
